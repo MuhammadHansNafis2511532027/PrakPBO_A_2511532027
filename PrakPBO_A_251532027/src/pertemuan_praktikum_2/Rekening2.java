f8e5d3ca87862ae75d25d9435ae7707553604831
DELL  5310 <DELL  5310@10.44.29.23>
2026-09-17 14:22:47 +0700

    Praktikum 2

---
+++ PrakPBO_A_251532027/src/pertemuan_praktikum_2/Rekening2.java
@@ -0,0 +1,64 @@
+package pertemuan_praktikum_2;
+
+import java.util.ArrayList;
+
+public class Rekening2 {
+	String nomorRekening;
+	String namaPemilik;
+	double saldo;
+	
+	// Implementasi Asosiasi (1-to-many)
+	ArrayList<Transaksi> riwayatTransaksi;
+	
+	public Rekening2(String nomor, String nama, double saldoAwal) {
+		this.nomorRekening = nomor;
+		this.namaPemilik = nama;
+		this.saldo = saldoAwal;
+		
+		//Wajib menginisialisasi ArrayList di dalam constructor agar tidak NullPointerException
+		this.riwayatTransaksi = new ArrayList<>();
+		
+		System.out.println("Rekening atas nama " + namaPemilik + " berhasil dibuat.");
+		
+		// ... (pertahankan method getNomorRekening() dan cekInformasi() yang sudah ada)
+		
+		if (saldo >= 50000) {
+			System.out.println("Rekening atas nama " + namaPemilik + " berhasil dibuat dengan saldo Rp" + saldo);
+		} else {
+			System.out.println("Gagal: setoran saldo awal minimal Rp 50000!");
+		}
+	}
+	
+	public void setorTunai(double nominal) {
+		if (nominal > 10000) {
+			saldo += nominal;
+			// Merekam riwayat (Pembuatan objek Transaksi di dalam method)
+			String idTrx = "TRX-S-" + System.currentTimeMillis();
+			Transaksi trxBaru = new Transaksi(idTrx, "Kredit", nominal);
+			riwayatTransaksi.add(trxBaru);
+			
+			System.out.println("Setor tunai Rp" + nominal + " berhasil. Saldo awal saat ini: Rp" + saldo);
+		} else {
+			System.out.println("Gagal: Nominal setor harus lebih dari 10000!");
+		}
+	}
+	
+	public void tarikTunai(double nominal) {
+		if (nominal < 10000) {
+			System.out.println("Transaksi Gagal : Minimal nominal penarikan 10.000");
+			} else if (nominal > saldo) {
+				System.out.println("Transaksi Gagal : Saldo tidak mencukupi. Saldo Anda: Rp" + saldo);
+			} else {
+				saldo -= nominal;
+				System.out.println("Tarik tunai Rp" + nominal + " berhasil. Saldo saat ini: Rp" + saldo);
+			}
+	}
+	
+	public void cekInformasi() {
+		System.out.println("--- INFO REKENING ---");
+		System.out.println("No. Rekening : " + nomorRekening);
+		System.out.println("Nama Pemilik : " + namaPemilik);
+		System.out.println("Saldo Akhir : Rp" + saldo);
+		System.out.println("-------------------");
+	}
+}
